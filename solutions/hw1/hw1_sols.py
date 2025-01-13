9fd59ef90b99566da4395af3db5c18ed05ce4e6e
abarton51 <abarton51@users.noreply.github.com>
2025-01-13 13:18:34 -0500

    vimtek testing, fix docker environment for homework solutions

--- solutions/hw1/hw1_sols.py
+++ solutions/hw1/hw1_sols.py
@@ -3,7 +3,6 @@ import importlib
 
 def run_question(question_module):
     try:
-        # Dynamically import the question module (e.g., q1.py)
         question = importlib.import_module(question_module)
         question.main()  # Assuming each question file has a main function
     except ModuleNotFoundError:
@@ -11,11 +10,14 @@ def run_question(question_module):
 
 def main():
     parser = argparse.ArgumentParser(description='Run Numerical Analysis Questions')
-    parser.add_argument('-q', '--question', type=str, required=True, help='The question to run (e.g., q1, q2, etc.)')
+    parser.add_argument('-q', '--question', type=str, required=True, nargs='+', 
+                        help='The questions to run (e.g., q1, q2, etc.)')
     args = parser.parse_args()
 
-    # Run the desired question module
-    run_question(args.question)
+    print(f"Running the following question modules: {args.question}")
+
+    for question in args.question:
+        run_question(question)
 
 if __name__ == "__main__":
     main()
---
+++ solutions/hw1/q3.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def main():
+    print("Solution for question 3\n-----------------------------")
+
+def convert_bin_to_dec(dec_code):
+    size = 5
+    low = -50
+    high = 50
+    cases = np.random.uniform(low, high, size)
+    print(f"Converting Decimal to Binary code for the following cases: {cases}")
+
+def convert_dec_to_bin(bin_code):
+    pass
+
+if __name__ == '__main__':
+    main()
+
