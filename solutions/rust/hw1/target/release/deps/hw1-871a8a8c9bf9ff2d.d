bb56257403c728ae92095738a10266a80495cab2
abarton51 <abarton51@users.noreply.github.com>
2025-01-24 14:00:08 -0500

    hw1 questino 4

--- solutions/rust/hw1/target/release/deps/hw1-871a8a8c9bf9ff2d.d
+++ solutions/rust/hw1/target/release/deps/hw1-871a8a8c9bf9ff2d.d
@@ -1,6 +1,10 @@
-/home/teddy/Documents/Academics/Math/Numerical/numerical_analysis_math4640/solutions/rust/hw1/target/release/deps/hw1-871a8a8c9bf9ff2d: src/main.rs src/q3.rs
+/home/teddy/Documents/Academics/Math/Numerical/numerical_analysis_math4640/solutions/rust/hw1/target/release/deps/hw1-871a8a8c9bf9ff2d: src/main.rs src/math/mod.rs src/math/approx.rs src/math/interpolation.rs src/q3.rs src/q4.rs
 
-/home/teddy/Documents/Academics/Math/Numerical/numerical_analysis_math4640/solutions/rust/hw1/target/release/deps/hw1-871a8a8c9bf9ff2d.d: src/main.rs src/q3.rs
+/home/teddy/Documents/Academics/Math/Numerical/numerical_analysis_math4640/solutions/rust/hw1/target/release/deps/hw1-871a8a8c9bf9ff2d.d: src/main.rs src/math/mod.rs src/math/approx.rs src/math/interpolation.rs src/q3.rs src/q4.rs
 
 src/main.rs:
+src/math/mod.rs:
+src/math/approx.rs:
+src/math/interpolation.rs:
 src/q3.rs:
+src/q4.rs:
