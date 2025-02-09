d74f8b1f0597c8d78d9b2981846839f02349b0e3
abarton51 <abarton51@users.noreply.github.com>
2025-02-09 12:30:38 -0500

    update hw1

---
+++ solutions/rust/hw2/src/main.rs
@@ -0,0 +1,27 @@
+use std::env;
+use std::process;
+
+mod math;
+
+const SEPARATOR_STR: &str = "---------------";
+
+fn run_question(question: &str) {
+    match question {
+        _ => {
+            println!("Error: Could not find the module {}", question);
+        }
+    }
+}
+
+fn main() {
+    println!("---MATH 4640, Numerical Analysis, Homework 1---");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Usage: hw1 <question>");
+        process::exit(1);
+    }
+
+    for question in args.iter().skip(1) {
+        run_question(question);
+    }
+}
