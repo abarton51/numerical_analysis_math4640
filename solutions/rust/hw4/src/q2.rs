f7916a40c92b28d4863612336c3c202329916d07
abarton51 <abarton51@users.noreply.github.com>
2025-04-14 10:39:50 -0400

    hw4 setup for question 2

---
+++ solutions/rust/hw4/src/q2.rs
@@ -0,0 +1,10 @@
+fn solve_q2() -> Result<(), Box<dyn std::error::Error>> {
+    Ok(())
+}
+
+pub fn main() {
+    if let Err(e) = solve_q2() {
+        eprintln!("Error: {}", e);
+        std::process::exit(1);
+    }
+}
