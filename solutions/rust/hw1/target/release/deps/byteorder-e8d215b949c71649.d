1c8ee90ab5bc23ea6ceb1a336736141e3cb24a55
abarton51 <abarton51@users.noreply.github.com>
2025-04-21 09:43:15 -0400

    clean up

--- solutions/rust/hw1/target/release/deps/byteorder-e8d215b949c71649.d
+++
@@ -1,7 +0,0 @@
-/home/teddy/Documents/Academics/Math/Numerical/numerical_analysis_math4640/solutions/rust/hw1/target/release/deps/libbyteorder-e8d215b949c71649.rmeta: /home/teddy/.cargo/registry/src/index.crates.io-6f17d22bba15001f/byteorder-1.5.0/src/lib.rs
-
-/home/teddy/Documents/Academics/Math/Numerical/numerical_analysis_math4640/solutions/rust/hw1/target/release/deps/libbyteorder-e8d215b949c71649.rlib: /home/teddy/.cargo/registry/src/index.crates.io-6f17d22bba15001f/byteorder-1.5.0/src/lib.rs
-
-/home/teddy/Documents/Academics/Math/Numerical/numerical_analysis_math4640/solutions/rust/hw1/target/release/deps/byteorder-e8d215b949c71649.d: /home/teddy/.cargo/registry/src/index.crates.io-6f17d22bba15001f/byteorder-1.5.0/src/lib.rs
-
-/home/teddy/.cargo/registry/src/index.crates.io-6f17d22bba15001f/byteorder-1.5.0/src/lib.rs:
--- solutions/rust/hw1/target/release/deps/unicode_ident-17278dafa5f26de9.d
+++
@@ -1,8 +0,0 @@
-/home/teddy/Documents/Academics/Math/Numerical/numerical_analysis_math4640/solutions/rust/hw1/target/release/deps/libunicode_ident-17278dafa5f26de9.rmeta: /home/teddy/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.14/src/lib.rs /home/teddy/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.14/src/tables.rs
-
-/home/teddy/Documents/Academics/Math/Numerical/numerical_analysis_math4640/solutions/rust/hw1/target/release/deps/libunicode_ident-17278dafa5f26de9.rlib: /home/teddy/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.14/src/lib.rs /home/teddy/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.14/src/tables.rs
-
-/home/teddy/Documents/Academics/Math/Numerical/numerical_analysis_math4640/solutions/rust/hw1/target/release/deps/unicode_ident-17278dafa5f26de9.d: /home/teddy/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.14/src/lib.rs /home/teddy/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.14/src/tables.rs
-
-/home/teddy/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.14/src/lib.rs:
-/home/teddy/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.14/src/tables.rs:
